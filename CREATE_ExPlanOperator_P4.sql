5ace828893b27ecd726f5952b96055c3ed177a93
alekz123 <aleks03@yahoo.com>
2021-01-25 01:38:48 -0600

    My first commit

---
+++ CREATE_ExPlanOperator_P4.sql
@@ -0,0 +1,26 @@
+CREATE TABLE ExPlanOperator_P4 (
+  ID INT IDENTITY(1, 1)
+  ,CX_Name VARCHAR(50)
+  ,CX_PhoneNum VARCHAR(50)
+  ,CX_Address VARCHAR(MAX)
+  ,CX_Credit INT
+  )
+GO
+ 
+INSERT INTO ExPlanOperator_P4
+VALUES (
+  'Alen'
+  ,'9625788954'
+  ,'London'
+  ,500
+  ) 
+ GO 1000
+ 
+INSERT INTO ExPlanOperator_P4
+VALUES (
+  'Frank'
+  ,'962445785'
+  ,'Germany'
+  ,1400
+  ) 
+ GO 1000
